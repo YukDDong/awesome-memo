503c943127694814e5fd32290d98263b7cce6351
YukDDong <ekfhd5537@naver.com>
2023-06-07 19:32:10 +0900

    CRUD의 Create 과정

---
+++ static/app.js
@@ -0,0 +1,24 @@
+const createMemo = async (value) => {
+  const res = await fetch("/memos", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      id: new Date(),
+      content: value,
+    }),
+  });
+  const jsonRes = await res.json();
+  console.log(jsonRes);
+};
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const input = document.querySelector("#memo-input");
+  createMemo(input.value);
+  input.value = "";
+};
+
+const form = document.querySelector("#memo-form");
+form.addEventListener("submit", handleSubmit);
