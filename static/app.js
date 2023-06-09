0cf2336a808082695bbb728b8564a58d31b22ecf
YukDDong <ekfhd5537@naver.com>
2023-06-10 02:33:55 +0900

    CRUD과정의 Read와 Update

--- static/app.js
+++ static/app.js
@@ -1,3 +1,39 @@
+const editMemo = async (event) => {
+  const id = event.target.dataset.id;
+  const editInput = prompt("수정할 값을 입력하세요!");
+  const res = await fetch(`/memos/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      id,
+      content: editInput,
+    }),
+  });
+  readMemo();
+};
+
+const displayMemo = (memo) => {
+  const ul = document.querySelector("#memo-ul");
+  const li = document.createElement("li");
+  const btn = document.createElement("button");
+  li.innerText = `[id: ${memo.id}] ${memo.content}`;
+  btn.innerText = "수정하기";
+  btn.addEventListener("click", editMemo);
+  btn.dataset.id = memo.id;
+  li.appendChild(btn);
+  ul.appendChild(li);
+};
+
+const readMemo = async () => {
+  const res = await fetch("/memos");
+  const jsonRes = await res.json();
+  const ul = document.querySelector("#memo-ul");
+  ul.innerHTML = "";
+  jsonRes.forEach(displayMemo);
+};
+
 const createMemo = async (value) => {
   const res = await fetch("/memos", {
     method: "POST",
@@ -5,12 +41,11 @@ const createMemo = async (value) => {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      id: new Date(),
+      id: new Date().getTime(),
       content: value,
     }),
   });
-  const jsonRes = await res.json();
-  console.log(jsonRes);
+  readMemo();
 };
 
 const handleSubmit = (event) => {
@@ -22,3 +57,5 @@ const handleSubmit = (event) => {
 
 const form = document.querySelector("#memo-form");
 form.addEventListener("submit", handleSubmit);
+
+readMemo();
